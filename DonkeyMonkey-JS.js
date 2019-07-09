85f93f3bec15a30b29558474dc737d18b89994f9
MakoGS <52493730+MakoGS@users.noreply.github.com>
2019-07-09 14:12:45 +0200

    add exercise

---
+++ DonkeyMonkey-JS.js
@@ -0,0 +1,19 @@
+/*Print the numbers 1 through 50.
+If the number is divisible by 7, you must skip the next number.
+If the number is divisible by 10 or 15, you must print “Donkey!”.
+If the number is not divisible by 2 and the previous number is divisible by 10, you must print “Monkey!”.*/
+
+for (let i = 1; i <= 50; i++){
+  if (i % 7 === 0) { 
+    continue; 
+  }
+  else if (i % 10 === 0 || i % 15 === 0) {
+  console.log("Donkey!");
+  }
+  else if (i % 2 !== 0 && (i-1) % 10 === 0 ) {
+    console.log("Monkey!")
+  }
+  else {
+    console.log(i)
+  }
+}
